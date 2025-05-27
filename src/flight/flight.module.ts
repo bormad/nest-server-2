8deaa8ffaf3e5f9a14d32a4d7429c22a214b6c5e
Vova <vacallationf@gmail.com>
2025-05-27 19:58:05 +0800

    fix flight

--- src/flight/flight.module.ts
+++ src/flight/flight.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { FlightController } from './flight.controller';
 import { FlightService } from './flight.service';
 
 @Module({
+	imports: [UserModule],
 	controllers: [FlightController],
 	providers: [FlightService],
 })
